280256a0f6f95aeace0aa42ba6788bd35dc15308
diaclavijo <david@diazclavijo.com>
2014-08-06 08:55:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkplaceSystems::Application.config.secret_key_base = '5f631498e231cbab43016a6eb6bd366128321cd50208935f5216d71057c8c2e23cca8c15750e623935648d9356a4ed3b94232152e025ef65c717c75c1b5b18ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorkplaceSystems::Application.config.session_store :cookie_store, key: '_workplace-systems_session'
